4175f1ba7cd10afeea1fa08d34a5de39de851c29
hamimahmad <hamimahmad027@gmail.com>
2025-10-24 22:53:07 +0600

    first part done

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,15 @@ import Navbar from './Components/Navbar'
 import Btn from './Components/Btn'
 import Footer from './Components/Footer'
 import Aboutuslastpart from './Pages/Hamim-Regwan/Aboutpage/Aboutuslastpart'
+import Aboutusfirstpart from './Pages/Hamim-Regwan/Aboutus1stpart/Aboutusfirstpart'
 
 
 const App = () => {
   return (
       <div>
           <Navbar />
-          <Btn>Call us</Btn>
+
+          <Aboutusfirstpart/>
           <Aboutuslastpart/>
           <Footer/>
     </div>
