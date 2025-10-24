db02f227b28049279314ee52ede97e0cd4ab8c77
riatcorder <raisulislamriat007@gmail.com>
2025-10-24 23:12:04 +0600

    dark mood baki

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,12 @@ import React from 'react'
 import Navbar from './Components/Navbar'
 import Btn from './Components/Btn'
 import Footer from './Components/Footer'
-
+import Riat from './Pages/Riat/Riat.jsx'
 const App = () => {
   return (
       <div>
           <Navbar />
+          <Riat/> 
           <Footer/>
     </div>
   )
--- src/Pages/Riat/Riat.jsx
+++ src/Pages/Riat/Riat.jsx
@@ -1,20 +1,225 @@
 import React from 'react'
 import Container from '../../Components/Container'
-
+import { BiSolidRightArrow } from "react-icons/bi";
+import { RxCross1 } from "react-icons/rx";
+import { FiPlus } from "react-icons/fi";
 const Riat = () => {
   return (
     <div>
       <Container>
-        <div className="text-center px-100">
-          <h1 className='font-Poppins text-5xl font-semibold py-4'>Our Pricing Plans</h1>
+        <div className="text-center lg:pt-40 lg:px-100 lg px-50">
+          <h1 className='font-Poppins lg:text-5xl text-3xl font-semibold lg:py-4 pt-4 py-2'>Our Pricing Plans</h1>
           <p className='font-Poppins text-[14px]'>When you’re ready to go beyond prototyping in Figma, Webflow is ready to help you bring your designs to life — without coding them.</p>
         </div>
-        <div className="">
-          <div className="w-1/3 p-4">
-            <div className=""></div>
+        <div className="flex gap-10 py-25">
+          <div className="w-1/3 lg:px-10 lg:py-25 px-3 pt-10 pb-20 bg-[#F4F6FC] hover:bg-[#1C1E53] hover:text-[#F4F6FC] rounded-2xl transition delay-50 duration-300 ease-in-out hover:-translate-y-1 hover:scale-105 cursor-pointer">
+            <div className="">
+              <div className="flex gap-2 items-center">
+                <h2 className='font-Poppins lg:text-3xl text-2xl font-semibold'>$299</h2>
+                <h5 className='font-Poppins lg:text-[14px] text-[12px] text-Royal-Blue'>Per Design</h5>
+              </div>
+              <div className="">
+                <h3 className='font-Poppins text-xl font-medium pt-5 lg:pb-3 pb-2'>Landing Page </h3>
+                <p className='font-Poppins text-[14px]'>When you’re ready to go beyond prototyping in Figma, </p>
+              </div>
+              <div className="lg:py-15 py-10">
+                <ul>
+            <li className='flex gap-2 items-center py-1'>
+              <div className="text-[#97DAAD]">
+                <BiSolidRightArrow/>
+              </div>
+              <p>All limited links</p>
+            </li>
+            <li className='flex gap-2 items-center py-1'>
+              <div className="text-[#97DAAD]">
+                <BiSolidRightArrow/>
+              </div>
+              <p>Own analytics platform</p>
+            </li>
+            <li className='flex gap-2 items-center py-1'>
+              <div className="text-[#97DAAD]">
+                <BiSolidRightArrow/>
+              </div>
+              <p>Chat support</p>
+            </li>
+            <li className='flex gap-2 items-center py-1'>
+              <div className="text-[#97DAAD]">
+                <BiSolidRightArrow/>
+              </div>
+              <p>Optimize hashtags</p>
+            </li>
+            <li className='flex gap-2 items-center py-1'>
+              <div className="text-[#97DAAD]">
+                <BiSolidRightArrow/>
+              </div>
+              <p>Unlimited users</p>
+            </li>
+          </ul>
+              </div>
+              <div className="text-center pt-10">
+                <a className='px-10 py-3 rounded-full hover:bg-[#FCD980] text-[#F4F6FC] hover:text-[#282938] bg-[#282938]' href="#">Get started</a>
+              </div>
+            </div>
           </div>
-          <div className="w-1/3 p-4"></div>
-          <div className="w-1/3 p-4"></div>
+          <div className="w-1/3 lg:px-10 lg:py-25 px-3 pt-10 pb-20 bg-[#F4F6FC] hover:bg-[#1C1E53] hover:text-[#F4F6FC] rounded-2xl transition delay-50 duration-300 ease-in-out hover:-translate-y-1 hover:scale-105 cursor-pointer">
+            <div className="">
+              <div className="flex gap-2 items-center">
+                <h2 className='font-Poppins lg:text-3xl text-2xl font-semibold'>$299</h2>
+                <h5 className='font-Poppins lg:text-[14px] text-[12px] text-Royal-Blue'>Per Design</h5>
+              </div>
+              <div className="">
+                <h3 className='font-Poppins text-xl font-medium pt-5 lg:pb-3 pb-2'>Landing Page </h3>
+                <p className='font-Poppins text-[14px]'>When you’re ready to go beyond prototyping in Figma, </p>
+              </div>
+              <div className="lg:py-15 py-10">
+                <ul>
+            <li className='flex gap-2 items-center py-1'>
+              <div className="text-[#97DAAD]">
+                <BiSolidRightArrow/>
+              </div>
+              <p>All limited links</p>
+            </li>
+            <li className='flex gap-2 items-center py-1'>
+              <div className="text-[#97DAAD]">
+                <BiSolidRightArrow/>
+              </div>
+              <p>Own analytics platform</p>
+            </li>
+            <li className='flex gap-2 items-center py-1'>
+              <div className="text-[#97DAAD]">
+                <BiSolidRightArrow/>
+              </div>
+              <p>Chat support</p>
+            </li>
+            <li className='flex gap-2 items-center py-1'>
+              <div className="text-[#97DAAD]">
+                <BiSolidRightArrow/>
+              </div>
+              <p>Optimize hashtags</p>
+            </li>
+            <li className='flex gap-2 items-center py-1'>
+              <div className="text-[#97DAAD]">
+                <BiSolidRightArrow/>
+              </div>
+              <p>Unlimited users</p>
+            </li>
+          </ul>
+              </div>
+              <div className="text-center pt-10">
+                <a className='px-10 py-3 rounded-full hover:bg-[#FCD980] text-[#F4F6FC] hover:text-[#282938] bg-[#282938]' href="#">Get started</a>
+              </div>
+            </div>
+          </div>
+          <div className="w-1/3 lg:px-10 lg:py-25 px-3 pt-10 pb-20 bg-[#F4F6FC] hover:bg-[#1C1E53] hover:text-[#F4F6FC] rounded-2xl transition delay-50 duration-300 ease-in-out hover:-translate-y-1 hover:scale-105 cursor-pointer">
+            <div className="">
+              <div className="flex gap-2 items-center">
+                <h2 className='font-Poppins lg:text-3xl text-2xl font-semibold'>$299</h2>
+                <h5 className='font-Poppins lg:text-[14px] text-[12px] text-Royal-Blue'>Per Design</h5>
+              </div>
+              <div className="">
+                <h3 className='font-Poppins text-xl font-medium pt-5 lg:pb-3 pb-2'>Landing Page </h3>
+                <p className='font-Poppins text-[14px]'>When you’re ready to go beyond prototyping in Figma, </p>
+              </div>
+              <div className="lg:py-15 py-10">
+                <ul>
+            <li className='flex gap-2 items-center py-1'>
+              <div className="text-[#97DAAD]">
+                <BiSolidRightArrow/>
+              </div>
+              <p>All limited links</p>
+            </li>
+            <li className='flex gap-2 items-center py-1'>
+              <div className="text-[#97DAAD]">
+                <BiSolidRightArrow/>
+              </div>
+              <p>Own analytics platform</p>
+            </li>
+            <li className='flex gap-2 items-center py-1'>
+              <div className="text-[#97DAAD]">
+                <BiSolidRightArrow/>
+              </div>
+              <p>Chat support</p>
+            </li>
+            <li className='flex gap-2 items-center py-1'>
+              <div className="text-[#97DAAD]">
+                <BiSolidRightArrow/>
+              </div>
+              <p>Optimize hashtags</p>
+            </li>
+            <li className='flex gap-2 items-center py-1'>
+              <div className="text-[#97DAAD]">
+                <BiSolidRightArrow/>
+              </div>
+              <p>Unlimited users</p>
+            </li>
+          </ul>
+              </div>
+              <div className="text-center pt-10">
+                <a className='px-10 py-3 rounded-full hover:bg-[#FCD980] text-[#F4F6FC] hover:text-[#282938] bg-[#282938]' href="#">Get started</a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="lg:flex">
+          <div className="lg:w-1/3 px-3">
+          <h2 className='font-Poppins lg:text-5xl text-4xl py-5 '>Frequently asked questions</h2>
+          <p className='font-Poppins text-[14px] text-Royal-Blue'>Contact us For More Info</p>
+          </div>
+          <div className="lg:w-2/3 lg:py-0 py-10 px-10">
+          <div className="flex justify-between border-b-2 border-Royal-Blue pb-8">
+            <p className='font-Poppins text-Royal-Blue'>01</p>
+            <div className="pl-20 pr-30">
+              <h5 className='font-Poppins text-2xl'>How much time does it take ?</h5>
+              <p className='py-3'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            </div>
+            <div className="pt-2">
+              <RxCross1/>
+            </div>
+          </div>
+           <div className="flex items-center border-b-2 border-Royal-Blue pb-8 pt-8">
+            <div className="w-3/22">
+              <p className='font-Poppins  text-Royal-Blue font-semibold '>02</p>
+            </div>
+              <div className="w-18/22">
+                <h5 className='font-Poppins text-2xl w-full '>What is your class naming convention ?</h5>
+              </div>
+            <div className="w-1/22">
+              <FiPlus/>
+            </div>
+          </div>
+           <div className="flex items-center border-b-2 border-Royal-Blue pb-8 pt-8">
+            <div className="w-3/22">
+              <p className='font-Poppins text-Royal-Blue font-semibold'>03</p>
+            </div>
+            <div className="w-18/22">
+              <h5 className='font-Poppins text-2xl '>How do we communicate ?</h5>
+            </div>
+            <div className="1/22">
+              <FiPlus/>
+            </div>
+          </div>
+           <div className="flex items-center border-b border-Royal-Blue pb-8 pt-8">
+            <div className="w-3/22">
+              <p className='font-Poppins text-Royal-Blue font-semibold'>04</p>
+            </div>
+              <h5 className='font-Poppins text-2xl w-18/22'>I have a bigger project. Can you handle it ?</h5>
+            <div className="1/22">
+              <FiPlus/>
+            </div>
+          </div>
+           <div className="flex items-center border-b border-Royal-Blue pb-8 pt-8">
+            <div className="w-3/22">
+              <p className='font-Poppins text-Royal-Blue font-semibold'>05</p>
+            </div>
+            <div className="w-18/22">
+              <h5 className='font-Poppins text-2xl '>What is your class naming convention ? </h5>
+            </div>
+            <div className="w-1/22">
+              <FiPlus/>
+            </div>
+          </div>
+
+          </div>
         </div>
       </Container>
     </div>
