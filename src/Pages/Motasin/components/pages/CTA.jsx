34fe257c8f2d3b16523339b31ed9493130a79153
motasimdev <themotasimdev@gmail.com>
2025-10-24 22:46:31 +0600

    CTA part dark done

--- src/Pages/Motasin/components/pages/CTA.jsx
+++ src/Pages/Motasin/components/pages/CTA.jsx
@@ -5,12 +5,12 @@ import Btn from '../Btn'
 const CTA = () => {
   return (
     <>
-    <section className='md:py-[60px]'>
+    <section className='md:py-[60px] dark:bg-Dark-Blue'>
         <Container>
             <div className="text-center">
-                <h3 className='w-[360px] md:w-[630px] lg:w-[730px] m-auto text-[28px] md:text-[38] lg:text-[48px] font-Poppins font-semibold text-Dark-Blue'>Let's build something great together</h3>
-                <p className='w-[260px] md:w-[460px] lg:w-[560px] m-auto pt-7 pb-10 text-sm font-Poppins font-medium text-Dark-Blue'>Nullam vitae purus at tortor mattis dapibus. Morbi purus est, ultricies nec dolor sit amet, scelerisque cursus purus.</p>
-                <button className='lg:px-17 px-7 lg:py-4 py-3 text-Dark-Blue bg-[#FCD980] hover:bg-Dark-Blue hover:text-white font-Manrope font-semibold lg:text-[18px] text-sm leading-8 rounded-4xl duration-500 ease-in-out cursor-pointer'>Contact Us</button>
+                <h3 className='w-[360px] md:w-[630px] lg:w-[730px] m-auto text-[28px] md:text-[38] lg:text-[48px] font-Poppins font-semibold dark:text-gray-50 text-Dark-Blue'>Let's build something great together</h3>
+                <p className='w-[260px] md:w-[460px] lg:w-[560px] m-auto pt-7 pb-10 text-sm font-Poppins font-medium dark:text-gray-50 text-Dark-Blue'>Nullam vitae purus at tortor mattis dapibus. Morbi purus est, ultricies nec dolor sit amet, scelerisque cursus purus.</p>
+                <button className='lg:px-17 px-7 lg:py-4 py-3 text-Dark-Blue bg-[#FCD980] hover:bg-Dark-Blue dark:hover:bg-gray-50 dark:hover:text-Dark-Blue hover:text-white font-Manrope font-semibold lg:text-[18px] text-sm leading-8 rounded-4xl duration-500 ease-in-out cursor-pointer'>Contact Us</button>
             </div>
         </Container>
     </section>
