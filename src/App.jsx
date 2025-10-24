5b38189a7c67cc65cd99fac39d8208febb251711
LitanMolla <litanmolla9@gmail.com>
2025-10-24 19:39:26 +0600

    clear app

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,13 @@ import React from 'react'
 import Navbar from './Components/Navbar'
 import Btn from './Components/Btn'
 import Footer from './Components/Footer'
-import Home from './Pages/Litan/Home/Home'
 
 const App = () => {
   return (
       <div>
-          {/* <Navbar /> */}
-          {/* <Btn>Call us</Btn> */}
-          <Home/>
-          {/* <Footer/> */}
+          <Navbar />
+          <Btn>Call us</Btn>
+          <Footer/>
     </div>
   )
 }
