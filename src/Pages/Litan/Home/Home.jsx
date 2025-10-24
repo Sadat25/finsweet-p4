fdeb9870518ea7c4211c6b97741b5a7762e3113f
LitanMolla <litanmolla9@gmail.com>
2025-10-24 18:29:08 +0600

    added FAQ section

--- src/Pages/Litan/Home/Home.jsx
+++ src/Pages/Litan/Home/Home.jsx
@@ -3,6 +3,7 @@ import HowWeWork from '../components/HowWeWork/HowWeWork'
 import OurProjects from '../components/OurProjects/OurProjects'
 import Features from '../components/Features/Features'
 import ClientsReview from '../components/ClientsReview/ClientsReview'
+import FAQ from '../components/FAQ/FAQ'
 
 const Home = () => {
   return (
@@ -11,6 +12,7 @@ const Home = () => {
         <OurProjects/>
         <Features/>
         <ClientsReview/>
+        <FAQ/>
     </div>
   )
 }
---
+++ src/Pages/Litan/components/FAQ/FAQ.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import FaqCard from '../FaqCard/FaqCard'
+
+const FAQ = () => {
+    return (
+        <div className='py-10 md:py-20 bg-white'>
+            <div className="container px-5 md:px-0">
+                <div className="flex justify-between gap-8 flex-col md:flex-row">
+                    <div className="md:w-4/12">
+                        <h4 className='text-4xl font-bold'>Frequently asked questions</h4>
+                        <a href="/" className='text-Royal-Blue font-medium mt-5 inline-block'>Contact us for more info</a>
+                    </div>
+                    <div className="md:w-8/12">
+                        <div className="space-y-2">
+                            <FaqCard
+                                serial='01'
+                                question='How much time does it take?'
+                                answer='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+                            />
+                            <FaqCard
+                                serial='02'
+                                question='What is your class naming convention?'
+                                answer='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+                            />
+                            <FaqCard
+                                serial='03'
+                                question='How do you communicate?'
+                                answer='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+                            />
+                            <FaqCard
+                                serial='04'
+                                question='I have a bigger project. Can you handle it?'
+                                answer='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+                            />
+                            <FaqCard
+                                serial='05'
+                                question='What is your class naming convention?'
+                                answer='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+                            />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default FAQ
