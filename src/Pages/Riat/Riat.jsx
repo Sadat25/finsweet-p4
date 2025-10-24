ac7e6a7db467b827bd433ce3d381faa50a5dc0ab
Raisul-0007 <raisulislamriat007@gmail.com>
2025-10-24 14:51:31 +0600

    Heading

---
+++ src/Pages/Riat/Riat.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import Container from '../../Components/Container'
+
+const Riat = () => {
+  return (
+    <div>
+      <Container>
+        <div className="text-center px-100">
+          <h1 className='font-Poppins text-5xl font-semibold py-4'>Our Pricing Plans</h1>
+          <p className='font-Poppins text-[14px]'>When you’re ready to go beyond prototyping in Figma, Webflow is ready to help you bring your designs to life — without coding them.</p>
+        </div>
+        <div className="">
+          <div className="w-1/3 p-4">
+            <div className=""></div>
+          </div>
+          <div className="w-1/3 p-4"></div>
+          <div className="w-1/3 p-4"></div>
+        </div>
+      </Container>
+    </div>
+  )
+}
+
+export default Riat
