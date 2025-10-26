1fb2735e8a940547104bfedf03a2096c42a47d1e
Ahnaf Tahmid <ahnaf.ent@gmail.com>
2025-10-26 19:08:27 +0600

    update responsive contact page

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,10 +1,10 @@
-import React, { useState, useEffect } from 'react' 
+import React, { useState, useEffect } from 'react'
 import { FaMoon, FaSun } from 'react-icons/fa6'
 
 const Contact = () => {
   const [show, setShow] = useState(false)
 
-  // Toggle dark mode
+  
   useEffect(() => {
     if (show) {
       document.documentElement.classList.add('dark')
@@ -26,7 +26,7 @@ const Contact = () => {
 
       <div className='w-full py-5'>
         <div className='container'>
-          <div className='mx-auto px-4 lg:pt-[128px] md:-[60px]'> 
+          <div className='mx-auto px-4 lg:pt-[128px] md:-[60px]'>
             <h2 className='lg:text-[32px] md:text-[28px]   font-Poppins font-bold text-[#282938] dark:text-white text-center'>
               Contact Us
             </h2>
@@ -35,43 +35,43 @@ const Contact = () => {
             </p>
           </div>
 
-          <div className='w-full  mt-[64px] '>
-            <div className='lg:w-[1062px] lg:h-[640px] md:w-full md:h-[40px] bg-[#F4F6FC] dark:bg-Dark-Blue  mx-auto lg:flex lg:flex-wrap  justify-between  pt-[55px]'>
-              
-              <div className='lg:w-[438px] lg:h-[64px] md:w-full md:h-[40px] mx-auto '>
-                <h2 className='text-[18px] font-Poppins font-medium dark:text-white'>Name</h2>
-                <input className='w-full h-[64px] border border-[#D2D2E0] dark:border-gray-600 pl-5 text-[16px] font-Poppins font-medium rounded-lg transition duration-300  dark:bg-gray-800 dark:text-white' type="text" placeholder='Enter Your Name' />
+          <div className="w-full mt-[64px]">
+            <div className="bg-[#F4F6FC] dark:bg-Dark-Blue mx-auto lg:w-[1062px] rounded-lg px-4 sm:px-6 md:px-8 lg:px-10 py-[55px] flex flex-col gap-6 lg:flex-wrap lg:flex-row justify-between">
+
+
+              <div className="w-full  md:w-[90%] lg:w-[438px] mx-auto">
+                <h2 className="text-[18px] font-Poppins font-medium dark:text-white mb-2">Name</h2>
+                <input type="text" placeholder="Enter Your Name" className="w-full h-[64px] border border-[#D2D2E0] dark:border-gray-600 pl-5 text-[16px] font-Poppins font-medium rounded-lg transition duration-300 dark:bg-gray-800 dark:text-white" />
               </div>
 
-              <div className='lg:w-[438px] lg:h-[64px] md:w-full md:h-[40px] mx-auto'>
-                <h2 className='text-[18px] font-Poppins font-medium dark:text-white'>Email</h2>
-                <input className='w-full h-[64px] border border-[#D2D2E0] dark:border-gray-600 pl-5 text-[16px] font-Poppins font-medium rounded-lg transition duration-300 dark:bg-gray-800 dark:text-white'  type="text" placeholder='Enter your Email'/>
-              </div>
 
-              <div className='lg:w-[438px] lg:h-[64px] md:w-full md:h-[40px] mx-auto'>
-                <h2 className='text-[18px] font-Poppins font-medium dark:text-white'>Subject</h2>
-                <input className='w-full h-[64px] border border-[#D2D2E0] dark:border-gray-600 pl-5 text-[16px] font-Poppins font-medium rounded-lg transition duration-300 dark:bg-gray-800 dark:text-white' 
-                  type="text"  placeholder='Provide context'  />
-              </div>
+              <div className="w-full sm:w-[80%] md:w-[90%] lg:w-[438px] mx-auto">
+                <h2 className="text-[18px] font-Poppins font-medium dark:text-white mb-2">Email</h2>
+                <input type="text" placeholder="Enter your Email" className="w-full h-[64px] border border-[#D2D2E0] dark:border-gray-600 pl-5 text-[16px] font-Poppins font-medium rounded-lg transition duration-300 dark:bg-gray-800 dark:text-white" /> </div>
 
-              <div className='lg:w-[438px] lg:h-[64px] md:w-full md:h-[40px] mx-auto'>
-                <h2 className='text-[18px] font-Poppins font-medium dark:text-white'>Subject</h2>
-                <input  className='w-full h-[64px] border border-[#D2D2E0] dark:border-gray-600 pl-5 text-[16px] font-Poppins font-medium rounded-lg transition duration-300  dark:bg-gray-800 dark:text-white' 
-                  type="text"  placeholder='Select Subject' />
-              </div>
 
-              <div className='w-full lg:ml-[42px] md:ml-0'>
-                <h2 className='text-[18px] font-Poppins font-medium dark:text-white'>Message</h2>
-                <textarea className='lg:w-[908px] lg:h-[138px]  border border-[#D2D2E0] dark:border-gray-600 p-5 text-[16px] font-Poppins font-medium rounded-lg resize-none transition duration-300  dark:bg-gray-800 dark:text-white' placeholder='Write your question here' />
-              </div>
+              <div className="w-full  md:w-[90%] lg:w-[438px] mx-auto">
+                <h2 className="text-[18px] font-Poppins font-medium dark:text-white mb-2">Subject</h2>
+                <input type="text" placeholder="Provide context" className="w-full h-[64px] border border-[#D2D2E0] dark:border-gray-600 pl-5 text-[16px] font-Poppins font-medium rounded-lg transition duration-300 dark:bg-gray-800 dark:text-white" /> </div>
 
-              <div className='w-full py-5  '>
-                <a className='  px-8 py-3 lg:ml-[42px] md:ml-0 bg-[#282938] hover:bg-Royal-Blue text-white text-[16px] font-Poppins font-semibold rounded-full shadow-md transition duration-300 '
-                href="#">Send Message</a>
+
+              <div className="w-full  md:w-[90%] lg:w-[438px] mx-auto">
+                <h2 className="text-[18px] font-Poppins font-medium dark:text-white mb-2">Subject</h2>
+                <input type="text" placeholder="Select Subject" className="w-full h-[64px] border border-[#D2D2E0] dark:border-gray-600 pl-5 text-[16px] font-Poppins font-medium rounded-lg transition duration-300 dark:bg-gray-800 dark:text-white" /> </div>
+
+
+              <div className="w-full lg:ml-[12px] md:ml-0  mx-auto">
+                <h2 className="text-[18px] font-Poppins font-medium dark:text-white mb-2">Message</h2>
+                <textarea placeholder="Write your question here" className=" lg:w-full w-full h-[138px] border border-[#D2D2E0] dark:border-gray-600 p-5 text-[16px] font-Poppins font-medium transition duration-300 dark:bg-gray-800 dark:text-white" /> </div>
+
+
+              <div className="w-full py-5 lg:ml-[42px] md:ml-0  mx-auto">
+                <a href="#" className="inline-block px-8 py-3 bg-[#282938] hover:bg-Royal-Blue text-white text-[16px] font-Poppins font-semibold rounded-full shadow-md transition duration-300">Send Message</a>
               </div>
 
             </div>
           </div>
+
         </div>
       </div>
     </div>
